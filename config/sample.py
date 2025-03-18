81d88bd4e88a81c8ba7b541d217cc08800fd15ba
David Riccitelli <david@wordlift.io>
2025-03-18 16:26:42 +0200

    add sample configuration file

---
+++ config/sample.py
@@ -0,0 +1,6 @@
+# The sitemap.xml must contain `loc` items pointing to webpages URLs, not other sitemaps.
+import os
+
+SITEMAP_URL = "https://example.org/sitemap.xml"
+WORDLIFT_KEY = os.getenv('WORDLIFT_KEY')
+OUTPUT_TYPE = "http://schema.org/WebPage"
