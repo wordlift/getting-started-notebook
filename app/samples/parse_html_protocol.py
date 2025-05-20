f72d11d414520501b85ca731865beb4c6ee6ec31
David Riccitelli <david@wordlift.io>
2025-05-20 11:22:07 +0300

    fix base

--- app/samples/parse_html_protocol.py
+++ app/samples/parse_html_protocol.py
@@ -1,4 +1,7 @@
+from bs4 import BeautifulSoup
+from rdflib import URIRef, Literal
 from wordlift_client import EntityPatchRequest
+from wordlift_sdk.utils import create_entity_patch_request
 from wordlift_sdk.wordlift.sitemap_import.protocol import (
     ParseHtmlProtocolInterface,
     ParseHtmlInput,
@@ -9,4 +12,25 @@ class ParseHtmlProtocol(ParseHtmlProtocolInterface):
     async def parse_html(
         self, parse_html_input: ParseHtmlInput
     ) -> list[EntityPatchRequest]:
-        return list()
+        entity_id = parse_html_input.entity_id
+        html = parse_html_input.html
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        # Extract the text of all 'a' tags with class 'tag-cloud-link'
+        tag_texts = [
+            a.get_text(strip=True) for a in soup.find_all("a", class_="tag-cloud-link")
+        ]
+
+        resource = URIRef(entity_id)
+
+        payloads = []
+
+        for value in tag_texts:
+            payloads.append(
+                create_entity_patch_request(
+                    resource, URIRef("http://schema.org/keywords"), Literal(value)
+                )
+            )
+
+        return payloads
